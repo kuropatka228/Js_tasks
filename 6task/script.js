a76f3c56528b1bc9981b94c6e78e7fa6f244204a
kuropatka228 <egorshumilo25@gmail.com>
2024-02-18 01:26:15 +0900

    6-1

--- 6task/script.js
+++ 6task/script.js
@@ -1,1 +1,15 @@
-console.log('aboba')
+const getName1 = function(name){
+    return `Имя равно ${name}`;
+};
+
+function getName2(name){
+    return `Имя равно ${name}`;
+};
+
+const getName3 = (name) => `Имя равно ${name}`;
+
+console.log(getName1("Егор"));
+
+console.log(getName2("Игорь"));
+
+console.log(getName3("Ега"));
