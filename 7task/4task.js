e7064821731623749d675035310aacab82da99f2
kuropatka228 <egorshumilo25@gmail.com>
2024-04-08 13:34:20 +0900

    4

--- 7task/4task.js
+++ 7task/4task.js
@@ -1,2 +1,5 @@
 const coffees = ['cappucino', 'latee', 'americano']
 const prices = [1.5, 1, 2]
+const updatedPriсes = prices.map(price => price + 0.5)
+
+updatedPriсes.forEach((prices, index ) => {alert(`кофе ${coffees[index]} сейчас стоит ${prices} евро`)})
