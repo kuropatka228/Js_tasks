12bfe593cc953db840341bb79a94b02a01c0b3b0
kuropatka228 <egorshumilo25@gmail.com>
2024-04-08 15:09:24 +0900

    6

---
+++ 7task/6task.js
@@ -0,0 +1,24 @@
+// const numbers = [10, 4, 100, -5, 54, 2]
+// let sum = 0
+// for (let i = 0; i < numbers.length; i++){
+//     sum += Math.pow(numbers[i], 3)
+// }
+// console.log()
+
+// const numbers = [10, 4, 100, -5, 54, 2]
+// let sum = 0
+// for (const number of numbers){
+//     sum += Math.pow(number, 3)
+// }
+// console.log(sum)
+
+// const numbers = [10, 4, 100, -5, 54, 2]
+// let sum = 0
+// numbers.forEach(number => {
+//     sum += Math.pow(number, 3)
+// })
+// console.log(sum)
+
+// const numbers = [10, 4, 100, -5, 54, 2]
+// const sum = numbers.reduce((acc, number) => acc + Math.pow(number, 3) ,0)
+// console.log(sum)
